ee02cdd6a026145bcb1e1438665e3841cb143347
frank-rachel <73958149+frank-rachel@users.noreply.github.com>
2024-04-15 18:39:09 +0200

    w

--- src/Database/Types/IntegerType.php
+++ src/Database/Types/IntegerType.php
@@ -3,5 +3,17 @@ namespace TCG\Voyager\Database\Types;
 
 class IntegerType extends Type
 {
+    // Default conversion to database value
+    public function convertToDatabaseValue($value, AbstractPlatform $platform)
+    {
+        // Default implementation, override in child classes if needed
+        return $value;
+    }
 
+    // Default conversion to PHP value
+    public function convertToPHPValue($value, AbstractPlatform $platform)
+    {
+        // Default implementation, override in child classes if needed
+        return $value;
+    }
 }
