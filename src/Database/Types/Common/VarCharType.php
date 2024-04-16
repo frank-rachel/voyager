928c31ae075a1ebaf3acd17ef62e9eca52a7fc8c
frank-rachel <73958149+frank-rachel@users.noreply.github.com>
2024-04-16 23:32:15 +0200

    Update VarCharType.php

--- src/Database/Types/Common/VarCharType.php
+++ src/Database/Types/Common/VarCharType.php
@@ -1,15 +1,26 @@
 <?php
-
 namespace TCG\Voyager\Database\Types\Common;
 
-use Doctrine\DBAL\Types\StringType as DoctrineStringType;
+use TCG\Voyager\Database\Types\Type;
 
-class VarCharType extends DoctrineStringType
+class VarCharType extends Type
 {
     public const NAME = 'varchar';
 
-    public function getName()
+    // Define a default category or use the one in the base class if appropriate
+    protected $category = 'String';
+
+    public function getSQLDeclaration(array $field)
     {
-        return static::NAME;
+        $length = $field['length'] ?? 2000; // Default length
+        return "VARCHAR($length)";
+    }
+
+    // You could override `toArray()` if additional properties are needed
+    public function toArray()
+    {
+        return array_merge(parent::toArray(), [
+            'length' => 2000, // Default length, adjust as necessary or make it dynamic based on $field
+        ]);
     }
 }
