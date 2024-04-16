39c51351dcace5ac10b21aa3a207845a51fb2a34
frank-rachel <73958149+frank-rachel@users.noreply.github.com>
2024-04-16 20:23:34 +0200

    Update Table.php

--- src/Database/Schema/Table.php
+++ src/Database/Schema/Table.php
@@ -23,26 +23,26 @@ class Table
     {
         return $this->name;
     }
-	public function getColumns()
-	{
-		return $this->columns;
-	}
 
-	public function getIndexes()
-	{
-		return $this->indexes;
-	}
+    public function getColumns()
+    {
+        return $this->columns;
+    }
 
-	public function getForeignKeys()
-	{
-		return $this->foreignKeys;
-	}
+    public function getIndexes()
+    {
+        return $this->indexes;
+    }
 
-	public function getOptions()
-	{
-		return $this->options;
-	}
+    public function getForeignKeys()
+    {
+        return $this->foreignKeys;
+    }
 
+    public function getOptions()
+    {
+        return $this->options;
+    }
 
     public static function make($table)
     {
@@ -77,76 +77,69 @@ class Table
         return json_encode($this->toArray());
     }
 
+    public function getColumnsIndexes($columns, $sort = false)
+    {
+        if (!is_array($columns)) {
+            $columns = [$columns];
+        }
 
-    // Add other necessary methods and properties...
+        $matched = [];
+        foreach ($this->indexes as $index) {
+            if ($index->spansColumns($columns)) {
+                $matched[$index->getName()] = $index;
+            }
+        }
 
-	public function getColumnsIndexes($columns, $sort = false)
-	{
-		if (!is_array($columns)) {
-			$columns = [$columns];
-		}
+        if ($sort && count($matched) > 1) {
+            uasort($matched, [$this, 'compareIndexPriority']);
+        }
 
-		$matched = [];
-		foreach ($this->indexes as $index) {
-			if ($index->spansColumns($columns)) {
-				$matched[$index->getName()] = $index;
-			}
-		}
+        return $matched;
+    }
 
-		if ($sort && count($matched) > 1) {
-			uasort($matched, [$this, 'compareIndexPriority']);
-		}
+    protected function compareIndexPriority($index1, $index2)
+    {
+        $priorities = ['PRIMARY' => 3, 'UNIQUE' => 2, 'INDEX' => 1];
+        $priority1 = $priorities[$index1->getType()] ?? 0;
+        $priority2 = $priorities[$index2->getType()] ?? 0;
 
-		return $matched;
-	}
+        return $priority2 <=> $priority1; // Using PHP 7 spaceship operator for comparison
+    }
 
-	protected function compareIndexPriority($index1, $index2)
-	{
-		$priorities = ['PRIMARY' => 3, 'UNIQUE' => 2, 'INDEX' => 1];
-		$priority1 = $priorities[$index1->getType()] ?? 0;
-		$priority2 = $priorities[$index2->getType()] ?? 0;
-
-		return $priority2 <=> $priority1; // Using PHP 7 spaceship operator for comparison
-	}
-
-	public function diff(Table $compareTable)
-	{
-		// Custom logic to compare this table with another table
-		// Simplified example; you'd need to develop detailed comparison logic
-		$differences = [];
-		if ($this->name !== $compareTable->name) {
-			$differences['name'] = "Different names: {$this->name} vs {$compareTable->name}";
-		}
-		return $differences;
-	}
+    public function diff(Table $compareTable)
+    {
+        // Custom logic to compare this table with another table
+        $differences = [];
+        if ($this->name !== $compareTable->name) {
+            $differences['name'] = "Different names: {$this->name} vs {$compareTable->name}";
+        }
+        // More detailed comparison can be added here
+        return $differences;
+    }
 
     public function diffOriginal()
     {
         return (new Comparator())->diffTable(SchemaManager::getDoctrineTable($this->_name), $this);
     }
 
+    public function exportColumnsToArray()
+    {
+        return array_map(function($column) {
+            return $column->toArray();
+        }, $this->columns);
+    }
 
-	public function exportColumnsToArray()
-	{
-		return array_map(function($column) {
-			return $column->toArray();
-		}, $this->columns);
-	}
+    public function exportIndexesToArray()
+    {
+        return array_map(function($index) {
+            return $index->toArray() + ['table' => $this->name];
+        }, $this->indexes);
+    }
 
-	public function exportIndexesToArray()
-	{
-		return array_map(function($index) {
-			return $index->toArray() + ['table' => $this->name];
-		}, $this->indexes);
-	}
-
-	public function exportForeignKeysToArray()
-	{
-		return array_map(function($fk) {
-			return $fk->toArray();
-		}, $this->foreignKeys);
-	}
-
-
-
+    public function exportForeignKeysToArray()
+    {
+        return array_map(function($fk) {
+            return $fk->toArray();
+        }, $this->foreignKeys);
+    }
 }
