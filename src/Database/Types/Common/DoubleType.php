98b45f12d98df662da469fe7e2ff232cfb5524be
frank-rachel <73958149+frank-rachel@users.noreply.github.com>
2024-04-16 23:33:31 +0200

    Update DoubleType.php

--- src/Database/Types/Common/DoubleType.php
+++ src/Database/Types/Common/DoubleType.php
@@ -2,14 +2,38 @@
 
 namespace TCG\Voyager\Database\Types\Common;
 
-use Doctrine\DBAL\Types\FloatType as DoctrineFloatType;
+use TCG\Voyager\Database\Types\Type;
 
-class DoubleType extends DoctrineFloatType
+class DoubleType extends Type
 {
     public const NAME = 'double';
 
-    public function getName()
+    // Define a default category or use the one in the base class if appropriate
+    protected $category = 'Numeric';
+
+    public function getSQLDeclaration(array $field)
     {
-        return static::NAME;
+        // You might want to handle precision and scale if applicable
+        $precision = $field['precision'] ?? null;
+        $scale = $field['scale'] ?? null;
+        
+        $sql = "DOUBLE";
+        if ($precision !== null && $scale !== null) {
+            $sql .= " PRECISION($precision, $scale)";
+        } elseif ($precision !== null) {
+            $sql .= " PRECISION($precision)";
+        }
+
+        return $sql;
+    }
+
+    // You could override `toArray()` if additional properties are needed
+    public function toArray()
+    {
+        return array_merge(parent::toArray(), [
+            'precision' => null, // Default precision, adjust as necessary
+            'scale' => null      // Default scale, adjust as necessary
+        ]);
     }
 }
+
