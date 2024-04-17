910425a6f510d5e108c8623a254ba84aacb88c22
frank-rachel <73958149+frank-rachel@users.noreply.github.com>
2024-04-17 17:49:04 +0200

    w

--- src/Database/Types/Common/BigintType.php
+++ src/Database/Types/Common/BigintType.php
@@ -3,26 +3,16 @@ namespace TCG\Voyager\Database\Types\Common;
 
 use TCG\Voyager\Database\Types\Type;
 
-class BigintType extends Type
+class BigIntType extends Type
 {
-    public const NAME = 'bigint';
-
-    // Optionally set a default category
-    protected $category = 'Numeric';
-
-    public function getSQLDeclaration(array $field)
-    {
-        // Handling precision and scale for numeric types
-        // $precision = $field['precision'] ?? 10;  // Set default precision if not specified
-        // $scale = $field['scale'] ?? 0;           // Set default scale if not specified
-
-        return "bigint";
+    public static function getName() {
+        return 'bigint';
     }
 
-    // Override `toArray()` if additional properties are needed
-    public function toArray()
-    {
-        return (parent::toArray());
+    public function toArray() {
+        return [
+            'name' => self::getName(),
+            'category' => 'Numbers'  // Assuming bigint falls under this category
+        ];
     }
 }
-
