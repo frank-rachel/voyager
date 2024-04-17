2bd9e6e226a277ff92de9c8eceaa3a880b5b5e29
frank-rachel <73958149+frank-rachel@users.noreply.github.com>
2024-04-17 17:50:48 +0200

    Update BigintType.php

--- src/Database/Types/Common/BigintType.php
+++ src/Database/Types/Common/BigintType.php
@@ -5,14 +5,20 @@ use TCG\Voyager\Database\Types\Type;
 
 class BigIntType extends Type
 {
-    public static function getName() {
+    public const NAME = 'bigint';
+
+    // Optionally set a default category
+    protected $category = 'Numeric';
+
+
+    public function getName() {
         return 'bigint';
     }
 
     public function toArray() {
         return [
             'name' => self::getName(),
-            'category' => 'Numbers'  // Assuming bigint falls under this category
+            'category' => $category  
         ];
     }
 }
