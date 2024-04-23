9edc3a8d68143c15eff2acb0683ab42bbf2136b8
frank-rachel <73958149+frank-rachel@users.noreply.github.com>
2024-04-23 18:59:13 +0200

    Update SchemaManager.php

--- src/Database/Schema/SchemaManager.php
+++ src/Database/Schema/SchemaManager.php
@@ -113,22 +113,22 @@ abstract class SchemaManager
 			throw new \Exception("Table does not exist: $tableName");
 		}
 
-		// Get columns details
-		$columns = DB::select(DB::raw("SELECT column_name, data_type, is_nullable, column_default
-									   FROM information_schema.columns
-									   WHERE table_name = '{$tableName}'"));
+		// Get columns details, use parameter binding for safety
+		$columns = DB::select("SELECT column_name, data_type, is_nullable, column_default
+							   FROM information_schema.columns
+							   WHERE table_name = ?", [$tableName]);
 
 		// Get foreign keys
-		$foreignKeys = DB::select(DB::raw("SELECT tc.constraint_name, kcu.column_name, ccu.table_name AS foreign_table_name, ccu.column_name AS foreign_column_name
-										   FROM information_schema.table_constraints AS tc 
-										   JOIN information_schema.key_column_usage AS kcu ON tc.constraint_name = kcu.constraint_name
-										   JOIN information_schema.constraint_column_usage AS ccu ON ccu.constraint_name = tc.constraint_name
-										   WHERE tc.table_name = '{$tableName}' AND tc.constraint_type = 'FOREIGN KEY'"));
+		$foreignKeys = DB::select("SELECT tc.constraint_name, kcu.column_name, ccu.table_name AS foreign_table_name, ccu.column_name AS foreign_column_name
+								   FROM information_schema.table_constraints AS tc 
+								   JOIN information_schema.key_column_usage AS kcu ON tc.constraint_name = kcu.constraint_name
+								   JOIN information_schema.constraint_column_usage AS ccu ON ccu.constraint_name = tc.constraint_name
+								   WHERE tc.table_name = ? AND tc.constraint_type = 'FOREIGN KEY'", [$tableName]);
 
 		// Get indexes
-		$indexes = DB::select(DB::raw("SELECT indexname, indexdef 
-									   FROM pg_indexes 
-									   WHERE tablename = '{$tableName}'"));
+		$indexes = DB::select("SELECT indexname, indexdef 
+							   FROM pg_indexes 
+							   WHERE tablename = ?", [$tableName]);
 
 		// Convert database results into the appropriate structure
 		// You will need to create data transformation logic here based on your needs
